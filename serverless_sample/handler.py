83e311f6db01a140e62a77b25471b35dae4556fe
KoheiMori <koheimori@Mths20832.local>
2023-01-06 18:05:33 +0900

    serverless frameworkでREST APIとwebページのエンドポイントを作成

--- serverless_sample/handler.py
+++ serverless_sample/handler.py
@@ -1,5 +1,28 @@
 import json
 
+def index(event, context):
+    html = """<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    </head>
+    <body>
+    webpage
+    </body>
+    </html>"""
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html"
+        },
+        "body": html
+    }
+
+    return response
 
 def hello(event, context):
     body = {
